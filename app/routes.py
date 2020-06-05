9f2d5acaa20aae1282806760f828a9ea9d94535a
Peter Hardy <peterphardy@gmail.com>
2020-06-04 20:55:22 -0700

    starting to add login capabilities

--- app/routes.py
+++ app/routes.py
@@ -2,13 +2,14 @@
 from app import app
 from flask import render_template, jsonify
 from app.models import csv_importer
+from app.forms import LoginForm
 
 
 # Route for main page of bay area tracker exhibit carousels
 @app.route('/')
 def index():
     exhibits = csv_importer()
-    return render_template("index.html", exhibits=exhibits)
+    return render_template('index.html', exhibits=exhibits)
 
 
 # Route for Exhibits API
@@ -16,3 +17,16 @@ def index():
 def get_exhibits():
     exhibits = csv_importer()
     return jsonify({'exhibits': exhibits})
+
+
+# Route for About page
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+# Route for Login
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
